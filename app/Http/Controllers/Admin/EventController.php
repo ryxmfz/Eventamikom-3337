209ceaceb5a28118aa03ce286feb7ff39572077e
ryxmfz <nagaterbang965@gmail.com>
2026-04-17 15:07:15 +0700

    Tugas 2

---
+++ app/Http/Controllers/Admin/EventController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+    //
+    public function show()
+    {
+        return view('admin.events');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,30 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\EventController as AdminEventController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/profil', function () {
     return view('profil');
-});
-
+})->name('profil');
 Route::get('/katalog', function () {
     return view('katalog');
-});
-
+})->name('katalog');
 Route::get('/bantuan', function () {
     return view('bantuan');
-});
-
+})->name('bantuan');
 Route::get('/kontak', function () {
     return view('contact');
+})->name('kontak');
+
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
+Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/events', [AdminEventController::class, 'indexAdmin'])->name('events.index');
 });
