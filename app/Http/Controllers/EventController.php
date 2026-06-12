f07c2c3d0295d33204d3c5eb9913e4cacf43861f
ryxmfz <nagaterbang965@gmail.com>
2026-06-12 16:47:11 +0700

    tugas 10

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
 
 class EventController extends Controller
 {
-    //
+    public function index()
+    {
+
+        $events = Event::with('category')->latest()->get();
+        return view('welcome', compact('events'));
+    }
+
+    public function show(Event $event)
+    {
+       
+        return view('events.show', compact('event'));
+    }
 }
